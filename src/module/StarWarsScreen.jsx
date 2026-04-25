803768bf149ddad49402d96b3128282328a3a3ef
opspch <1962208-opspch@users.noreply.gitlab.com>
2026-04-25 16:38:17 +0200

    add star wars view, fetching data from swapi.info

---
+++ src/module/StarWarsScreen.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { Alert, Card, CardBody, CardHeader, Container, Spinner } from "reactstrap";
+import { DateTime } from "asab_webui_components";
+import axios from 'axios';
+
+function DetailRow({ label, children }) {
+	return (
+		<div className="row py-2 border-bottom">
+			<span className="col-sm-3 mb-0 text-body-secondary fw-bold ">{label}</span>
+			<span className="col-sm-9 mb-0">{children}</span>
+		</div>
+	);
+}
+
+export function StarWarsScreen() {
+	const [data, setData] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const controller = new AbortController();
+
+		async function loadData() {
+			setIsLoading(true);
+			setError(null);
+
+			try {
+				const response = await axios.get(
+					`https://swapi.info/api/films`,
+					{ signal: controller.signal }
+				);
+
+				setData(response.data);
+			} catch (e) {
+				if (e.name !== "CanceledError") {
+					setError(e);
+				}
+			} finally {
+				setIsLoading(false);
+			}
+		}
+
+		loadData();
+
+		return () => {
+			controller.abort();
+		};
+	}, []);
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	if (error) {
+		return <Alert color="danger">Failed to load data.</Alert>;
+	}
+
+	if (!data) {
+		return <Alert color="warning">Data was not found.</Alert>;
+	}
+
+	return (
+		<Container className="py-3">
+			<Link to="/">Back to index</Link>
+
+			{ data.map(ep =>
+                <Card className="mt-3" key={ep.episode_id}>
+                    <CardHeader>{ep.title}</CardHeader>
+                    <CardBody>
+                        <div className="mb-0">
+                            <DetailRow label="Episode number">{ep.episode_id}</DetailRow>
+                            <DetailRow label="Director">{ep.director}</DetailRow>
+                            <DetailRow label="Producer">{ep.producer}</DetailRow>
+                            <DetailRow label="Release Date"><DateTime value={ep.release_date} dateTimeFormat="MMMM d, yyyy" /></DetailRow>
+                            <DetailRow label="Description">{ep.opening_crawl}</DetailRow>
+                        </div>
+                    </CardBody>
+                </Card>
+            )}
+		</Container>
+	);
+}
--- src/module/index.jsx
+++ src/module/index.jsx
@@ -1,6 +1,7 @@
 import { Module } from "asab_webui_components";
 import { DetailScreen } from "./DetailScreen.jsx";
 import { TableScreen } from './TableScreen.jsx';
+import { StarWarsScreen } from './StarWarsScreen.jsx';
 
 export default class TableApplicationModule extends Module {
 	constructor(app, name) {
@@ -20,10 +21,23 @@ export default class TableApplicationModule extends Module {
 			component: DetailScreen,
 		});
 
+		app.Router.addRoute({
+			path: "/starwars",
+			end: false,
+			name: 'Star Wars Episodes',
+			component: StarWarsScreen,
+		});
+
 		app.Navigation.addItem({
 			name: "Table",
 			icon: 'bi bi-table',
 			url: "/",
+		});
+
+		app.Navigation.addItem({
+			name: "Star Wars",
+			icon: 'bi bi-table',
+			url: "/starwars",
 		});
 	}
 }
